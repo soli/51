331a480155fca906d22a7beeebddc34776ff2f66
Sylvain Soliman <Sylvain.Soliman@gmail.com>
2014-05-29 14:44:57 +0200

    StrongAI taking shape

--- 51.py
+++ 51.py
@@ -2,6 +2,7 @@
 import random
 import logging
 import argparse
+import sys
 
 
 faces = ['7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
@@ -47,19 +48,27 @@ class Player():
     def __str__(self):
         return cards_to_str(self.cards)
 
-    def play(self, heap, new):
+    def play(self, heap, last_card, new):
+        if last_card is not None:
+            self.mark_seen(last_card)
         logging.debug('hand before play: ' + str(self))
         select, val = self.select(heap)
         card, self.cards[select] = self.cards[select], new
         logging.debug('hand after play: ' + str(self))
+        self.mark_seen(new)
         print(card_to_str(card), end=' ')   # noqa
-        return val
+        return heap + val, card
 
     def select(self, heap):
         raise NotImplementedError('Default player is abstract')
 
+    def mark_seen(self, card):
+        pass
+
 
 class Random(Player):
+    # Random vs. Random
+    # about 50-40-10
     def select(self, heap):
         select = random.randrange(NCARDS)
         val = values[self.cards[select] % 8]
@@ -92,6 +101,10 @@ class Human(Player):
 
 
 class WeakAI(Player):
+    # WeakAI vs. WeakAI
+    # about 30-20-50
+    # WeakAI vs. Random
+    # about 97-1-2
     def __init__(self, deck):
         super().__init__(deck)
         # values that can lead to a 1-move win
@@ -101,9 +114,21 @@ class WeakAI(Player):
                 self.unsafe_values.extend([51 - val for val in v if val > 0])
             elif v > 0:
                 self.unsafe_values.append(51 - v)
-        self.unsafe_values.sort(reverse=True)
         logging.debug('Unsafe values: ' + str(self.unsafe_values))
+        # self.unsafe_values.sort(reverse=True)
         # assert self.unsafe_values == [50, 49, 48, 47, 44, 43, 41, 40]
+
+    def build_options(self, heap):
+        options = []
+        for select in range(NCARDS):
+            val = values[self.cards[select] % 8]
+            if isinstance(val, list):
+                options.extend([(v + heap, select) for v in val])
+            else:
+                options.append((val + heap, select))
+        logging.debug('full hand: ' + str(self))
+        logging.debug('corresponding options: ' + str(options))
+        return options
 
     def filter_options(self, options, func):
         filtered = [(k, v) for k, v in options if func(k, v)]
@@ -142,15 +167,7 @@ class WeakAI(Player):
         - cards with highest value
 
         This ordering is implemented by successive filtering.'''
-        options = []
-        for select in range(NCARDS):
-            val = values[self.cards[select] % 8]
-            if isinstance(val, list):
-                options.extend([(v + heap, select) for v in val])
-            else:
-                options.append((val + heap, select))
-        logging.debug('full hand: ' + str(self))
-        logging.debug('corresponding options: ' + str(options))
+        options = self.build_options(heap)
 
         # can we win?
         self.filter_win(options)
@@ -177,14 +194,63 @@ class WeakAI(Player):
         return (options[0][1], options[0][0] - heap)
 
 
+class WeakerAI(WeakAI):
+    # WeakerAI vs. WeakAI
+    # - without options.sort()
+    # about 12-23-65
+    # - without filter_singlevalued()
+    # about 35-45-20
+    # - without filter_duplicates()
+    # WeakerAI vs. WeakAI
+    # about 20-26-54
+    def select(self, heap):
+        '''Same as WeakAI without some filters'''
+        options = self.build_options(heap)
+        self.filter_win(options)
+        self.filter_nolose(options)
+        self.filter_safe(options)
+        self.filter_duplicates(options)
+        self.filter_singlevalued(options, heap)
+        options.sort(reverse=True)
+
+        logging.debug('remaining options: ' + str(options))
+        return (options[0][1], options[0][0] - heap)
+
+
+class StrongAI(WeakAI):
+    '''Same as WeakAI but counts seen cards to find new safe plays'''
+    # vs. WeakAI
+    # about 32-22-46
+    def __init__(self, deck):
+        super().__init__(deck)
+        self.seen = [0] * len(faces)
+        for c in self.cards:
+            self.mark_seen(c)
+
+    def mark_seen(self, card):
+        val = card % 8
+        self.seen[val] += 1
+        logging.debug('Cards seen: ' + str(self.seen))
+        if self.seen[val] == 4:
+            self.mark_safe(values[val])
+
+    def mark_safe(self, val):
+        if isinstance(val, list):
+            for v in val:
+                self.mark_safe(v)
+        elif val > 0:
+            self.unsafe_values.remove(51 - val)
+
+
 def get_subclasses(cls):
     for c in cls.__subclasses__():
         yield(c.__name__)
-        get_subclasses(c)
+        yield from get_subclasses(c)
 
 
 def game(player1, player2):
     heap = 0
+    last_card = None
     deck = Deck()
     # create instances corresponding to the class names given as arguments
     players = [globals()[player1](deck),
@@ -193,7 +259,7 @@ def game(player1, player2):
 
     current = 0
     while deck.drawn < 32 and heap < 51:
-        heap += players[current].play(heap, deck.draw(1))
+        heap, last_card = players[current].play(heap, last_card, deck.draw(1))
         print(heap)
         print()
         current = (current + 1) % len(players)
@@ -224,20 +290,22 @@ def main():
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
 
-    print('Cards\' values:')
-    print(
-        '7 = 7, 8 = 8, 9 = 0, T = 10 or -10, J = 2, Q = 3, K = 4, A = 1 or 11')
-    print()
+    if 'Human' in args.players:
+        print('Cards\' values:')
+        print('7 = 7, 8 = 8, 9 = 0, T = 10 or -10, J = 2, Q = 3, K = 4, ' +
+              'A = 1 or 11')
+        print()
 
     won = [0, 0, 0]
     for i in range(args.count):
         j = i % 2
-        result = game(args.players[i], args.players[1 - i])
+        result = game(args.players[j], args.players[1 - j])
         if result < 2 and j == 1:
             result = 1 - result
         won[result] += 1
 
-    print(won)
+    if args.count > 1:
+        print(won, file=sys.stderr)
 
 
 if __name__ == '__main__':
